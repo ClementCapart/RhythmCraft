dae0566b70effd3960297442c88ce53d3081150b
Clement Capart <clement.capart@gmail.com>
2015-11-12 08:03:34 +0000

    Input

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class Controller : MonoBehaviour
+{
+	private Controller m_instance = null;
+
+	private Direction m_currentDirection = Direction.None;
+	public Direction ControlDirection
+	{
+		get { return m_currentDirection; }
+	}
+
+	void Awake()
+	{
+		m_instance = this;
+	}
+
+	void Update()
+	{
+		if (XInput.GetButton(Buttons.LeftStickUp, 0))
+		{
+			m_currentDirection = Direction.Up;
+		}
+		else if (XInput.GetButton(Buttons.LeftStickRight, 0))
+		{
+			m_currentDirection = Direction.Right;
+		}
+		else if (XInput.GetButton(Buttons.LeftStickDown, 0))
+		{
+			m_currentDirection = Direction.Down;
+		}
+		else if (XInput.GetButton(Buttons.LeftStickLeft, 0))
+		{
+			m_currentDirection = Direction.Left;
+		}
+	}
+}
---
+++ Assets/Scripts/CraftToken.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction
+{
+	None,
+	Up, 
+	Right,
+	Down,
+	Left
+}
+
+public enum TokenType
+{
+	None,
+	Progress,
+	Quality,
+	Durability
+}
+
+public class CraftToken
+{
+	Direction	m_direction	= Direction.None;
+	TokenType	m_type		= TokenType.None;
+
+    float		m_timeLeft	= float.MaxValue;
+
+	public CraftToken(Direction direction, TokenType type, float timeLeft)
+	{
+		m_direction = direction;
+		m_type = type;
+		m_timeLeft = timeLeft;
+	}
+
+	public void SubUpdate()
+	{
+		m_timeLeft -= Time.deltaTime;
+		if(m_timeLeft <= 0.0f)
+		{
+			CheckValidation();
+		}
+	}
+
+	private void CheckValidation()
+	{
+		//If(Controller opposite Direction of direction
+		{
+			Validate();
+		}
+		//else
+		{
+			Destroy();
+		}
+	}
+
+	private void Validate()
+	{
+
+	}
+
+	private void Destroy()
+	{
+
+	}
+}
