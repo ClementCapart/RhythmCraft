0aaf726c447fc2f1aa792838ccec252133ded29c
Clement Capart <clement.capart@gmail.com>
2016-03-12 20:59:02 +0100

    Model Importer now import materials by Material Name

---
+++ Assets/Scripts/Editor/AssetCustomImporter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AssetCustomImporter : AssetPostprocessor 
+{
+    void OnPreprocessModel()
+    {
+        ModelImporter modelImporter = assetImporter as ModelImporter;
+
+        if(modelImporter)
+        {
+            modelImporter.materialName = ModelImporterMaterialName.BasedOnMaterialName;
+        }
+    }
+}
