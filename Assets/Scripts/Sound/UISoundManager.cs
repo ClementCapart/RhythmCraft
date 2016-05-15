7215da5dd07e43542c2c1f2751813ad28a48e0e6
Clement Capart <clement.capart@gmail.com>
2016-05-15 17:31:37 +0200

    Add Success/Fail Craft sounds

--- Assets/Scripts/Sound/UISoundManager.cs
+++ Assets/Scripts/Sound/UISoundManager.cs
@@ -7,7 +7,35 @@ public class UISoundManager : Singleton<UISoundManager>
     {
         if(Instance)
         {
-            
+            AkSoundEngine.PostEvent(eventName, Instance.gameObject);
         }
     }
+
+    void Awake()
+    {
+        CraftPatternPlayer.s_craftSequenceEnded += OnCraftSequenceEnded;
+    }
+    void Destroy()
+    {
+        CraftPatternPlayer.s_craftSequenceEnded -= OnCraftSequenceEnded;
+    }
+
+    private void OnCraftSequenceEnded(ItemData item, CraftState state)
+    {
+        switch(state)
+        {
+            case CraftState.Success:
+                PlaySound("Play_SuccessCraft");
+                break;
+
+            case CraftState.NearSuccess:
+            case CraftState.Failure:
+                PlaySound("Play_FailCraft");
+                break;
+        }
+    }
+
+    
+
+
 }
