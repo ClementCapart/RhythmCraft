542223a207bfea868eddec1b215d41d9a894c39a
Clement Capart <clement.capart@gmail.com>
2016-06-27 23:32:37 +0200

    Stored Text Speeds in Blackboard && Clean-up
    - Script on IntroTutorial prefab to set the different text speeds
    - Simple selection box to choose the speed in display text states
    - Prefab-ed most stuff : )

--- Assets/Scripts/StateMachine/DisplayTextBehaviourState.cs
+++ Assets/Scripts/StateMachine/DisplayTextBehaviourState.cs
@@ -1,12 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum TextDisplaySpeed
+{
+    Slow,
+    Medium,
+    Fast,
+    Instant
+}
+
 public class DisplayTextBehaviourState : StateMachineBehaviour 
 {
     [TextArea(3,10)]
     public string  m_TextToDisplay = "";
-    public float m_DisplaySpeed = 50.0f;
+    
+    public TextDisplaySpeed m_DisplaySpeed = TextDisplaySpeed.Medium;
     public string m_speechBubbleBlackboardId = "SpeechBubble";
+
+    public const string _DisplaySpeedSlowBlackboardId = "TextSlowSpeed";
+    public const string _DisplaySpeedMediumBlackboardId = "TextMediumSpeed";
+    public const string _DisplaySpeedFastBlackboardId = "TextFastSpeed";   
+ 
+    private float m_displaySpeedPicked = 0.0f;
 
     public Buttons m_ButtonToPress = Buttons.A;
     public bool m_DestroyBubbleOnEnd = false;
@@ -26,7 +41,38 @@ public class DisplayTextBehaviourState : StateMachineBehaviour
             StateMachineBlackboard.AddObject(animator, m_speechBubbleBlackboardId, m_speechUIElement);
         }
 
-        m_speechUIElement.DisplayText(m_TextToDisplay, m_DisplaySpeed);
+        switch(m_DisplaySpeed)
+        {
+            case TextDisplaySpeed.Slow:
+                if(!StateMachineBlackboard.GetFloat(animator, _DisplaySpeedSlowBlackboardId, out m_displaySpeedPicked))
+                {
+                    m_displaySpeedPicked = 0.0f;
+                    Debug.LogWarning("StateMachine: Displaying Text with no Display Speed stored in Blackboard, using Instant instead");
+                }
+                break;
+
+            case TextDisplaySpeed.Medium:
+                if(!StateMachineBlackboard.GetFloat(animator, _DisplaySpeedMediumBlackboardId, out m_displaySpeedPicked))
+                {
+                    m_displaySpeedPicked = 0.0f;
+                    Debug.LogWarning("StateMachine: Displaying Text with no Display Speed stored in Blackboard, using Instant instead");
+                }
+                break;
+
+            case TextDisplaySpeed.Fast:
+                if(!StateMachineBlackboard.GetFloat(animator, _DisplaySpeedFastBlackboardId, out m_displaySpeedPicked))
+                {
+                    m_displaySpeedPicked = 0.0f;
+                    Debug.LogWarning("StateMachine: Displaying Text with no Display Speed stored in Blackboard, using Instant instead");
+                }
+                break;
+
+            case TextDisplaySpeed.Instant:
+                m_displaySpeedPicked = 0.0f;
+                break;
+        }
+
+        m_speechUIElement.DisplayText(m_TextToDisplay, m_displaySpeedPicked);
 	}
 
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
---
+++ Assets/Scripts/StateMachine/DisplayTextSpeedsBlackboard.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayTextSpeedsBlackboard : MonoBehaviour 
+{
+    public Animator m_Animator = null;
+
+    public float m_SlowDisplaySpeed = 10.0f;
+    public float m_MediumDisplaySpeed = 30.0f;
+    public float m_FastDisplaySpeed = 60.0f;
+
+    void Start()
+    {
+        if(m_Animator != null)
+        {
+            StateMachineBlackboard.AddFloat(m_Animator, DisplayTextBehaviourState._DisplaySpeedSlowBlackboardId, m_SlowDisplaySpeed);
+            StateMachineBlackboard.AddFloat(m_Animator, DisplayTextBehaviourState._DisplaySpeedMediumBlackboardId, m_MediumDisplaySpeed);
+            StateMachineBlackboard.AddFloat(m_Animator, DisplayTextBehaviourState._DisplaySpeedFastBlackboardId, m_FastDisplaySpeed);
+        }
+    }
+
+    void OnValidate()
+    {
+        if(m_Animator != null)
+        {
+            StateMachineBlackboard.AddFloat(m_Animator, DisplayTextBehaviourState._DisplaySpeedSlowBlackboardId, m_SlowDisplaySpeed);
+            StateMachineBlackboard.AddFloat(m_Animator, DisplayTextBehaviourState._DisplaySpeedMediumBlackboardId, m_MediumDisplaySpeed);
+            StateMachineBlackboard.AddFloat(m_Animator, DisplayTextBehaviourState._DisplaySpeedFastBlackboardId, m_FastDisplaySpeed);
+        }
+    }
+
+    void Update()
+    {
+        OnValidate();
+    }
+}
